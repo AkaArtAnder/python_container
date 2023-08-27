c9a3c454fe0533bbd85abdc8ca258bf8b5208ff6
AkaArtAnder <andrey@endaltsev.ru>
2023-08-27 21:34:40 +0300

    add base project style format

---
+++ __main__.py
@@ -0,0 +1,4 @@
+from python_base import hello
+
+if __name__ == "__main__":
+    hello.print_text("Hello python_container")
---
+++ python_base/hello.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def print_text(text: str) -> None:
+    """Func printing input text
+
+    Args:
+        text (str): printing text
+    """
+    print(f'{text} ({sys.version})')
